a9ae5a3a7e558a3f22b3f56946ffd6e631d93361
1280061 <yugioh4335@gmail.com>
2021-07-06 12:22:27 +0900

    Won/lost message added

--- 1.c
+++ 1.c
@@ -1,13 +1,15 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 #include <time.h>
 int Dice();
 int main(){
+srand((unsigned int)time(NULL));
 printf("Rolling the dice...\n");
 
  int pa=0;
  int valu=0;
-
+ char yrname[20];
  while(pa<2)
  {
   valu += Dice(pa+1);
@@ -15,16 +17,22 @@ printf("Rolling the dice...\n");
  }
 
  printf("Total value:%d\n",valu);
+ if(valu>=7)
+  {
+   printf("%s won!\n",yrname);
+  }
+ else printf("%s lost!\n",yrname);
 
- return 0;
+return 0;
 
 }
+
 int Dice(int pa){
 
-  int dinum;
+ int dinum;
 
-  dinum = rand()%6+1;
-  printf("Die %d:%d\n",pa,dinum);
+ dinum = rand()%6+1;
+ printf("Die %d:%d\n",pa,dinum);
 
-  return dinum;
+ return dinum;
 }
